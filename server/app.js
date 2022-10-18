1fe2017482e8eb62b212d7a95e86ead7ab68686a
arshptl <arshpatel714@gmail.com>
2022-10-18 19:47:08 +0530

    added author rootQuery + graphiql in express app

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({
-    
-}));
+//graphqlHTTP is a middleware that has been passed into express app
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // passing this schema through this middleware
+    // giving express graphql to use this schema
+    schema: schema,
+    graphiql: true
+  })
+);
 
+// app is listening on this port
 app.listen(4000, () => {
   console.log("Now listening for reqs on port 4000");
 });
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,23 +1,80 @@
 const graphql = require("graphql");
+const find = require("lodash/find");
 
-const { GraphQLObjectType } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
+// dummy data
+var books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
+
+var authors = [
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" },
+];
+
+// types and schemas of the book object(Object Type)
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: graphql.GraphQLString },
-    name: { type: graphql.GraphQLString },
-    genre: { type: graphql.GraphQLString },
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
   }),
 });
 
+// types and schema of the author object(Object Type)
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
 
 // Root Queries, to get into the graphQL(graph) initially to grab data
-const rootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        book: {
-            
-        }
-    }
-})
+// how initially we jump into the graph
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    // Query name
+    book: {
+      // data of the query
+      type: BookType,
+      args: {
+        id: {
+          type: GraphQLID,
+        },
+      },
+      resolve(parent, args) {
+        // code to get data from db/other source
+        return find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {
+          type: GraphQLID,
+        },
+      },
+      resolve(parent, args) {
+        return find(authors, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
